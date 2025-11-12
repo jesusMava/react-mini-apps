6be3eef293386f1d34843c41645d0d72e106ee9b
jesusMava <jesus.macedo7@gmail.com>
2025-11-12 10:46:26 -0600

    Add rate and remove function

--- src/App.js
+++ src/App.js
@@ -6,21 +6,34 @@ import { useState } from 'react';
 import ColorList from './ColorList';
 
 function App() {
-  const [colors] = useState(colorData)
+  const [colors, setColors] = useState(colorData)
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
           and save to reload.
         <StarRating totalStars={5} 
           style={{backgoundColor: "lightblue"}}
           onDoubleClick={e => alert("Double clicked!")}
         />
 
-        <ColorList colors={colors} />
-
-        </p>
+        <ColorList
+          colors={colors}
+          onRemoveColor={(id) => {
+            const newColor = colors.filter(color => color.id !== id)
+            setColors(newColor)
+          }}
+          onRate={(id) =>{
+            setColors({...colors, id: {raiting: id}})
+          }}
+          onRateColor={(id, rating) => {
+            const newColors = colors.map(color =>
+              color.id === id ? { ...color, rating } : color
+            )
+            setColors(newColors)
+          }}
+        />
       </header>
 
     </div>
--- src/StarRating.js
+++ src/StarRating.js
@@ -1,25 +1,39 @@
 import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-const Star = ({ selected = false, onSelect = f => f }) => (
+const Star = ({ selected = false, onSelect = (f) => f }) => (
   <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
 );
 
 const createArray = (length) => [...Array(length)];
 
-export default function StarRating({ totalStars = 5, style={}, ...props}) {
-  const [selectedStars, setSelectedStars] = useState(3)
+export default function StarRating({
+  totalStars = 5,
+  style = {}, 
+  selectedStars = 0,
+  onRate = f => f,
+   ...props
+   }) {
+  // const [selectedStars, setSelectedStars] = useState(3);
 
   return (
-    <div style={{padding: "5px", ...style}} {...props}>
-      {createArray(totalStars).map((n,i) => (
-        <Star 
-        key={i}
-        selected={selectedStars > i }
-        onSelect={() => setSelectedStars(i +1)}
-        />
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star key={i} selected={selectedStars > i} onSelect={() => onRate(i + 1)}/>
       ))}
-      <p>{selectedStars} of {totalStars} stars</p>
-    </div>
-  )
+      <p>
+        {selectedStars} of {totalStars} stars
+        </p>
+    </>
+    // <div style={{padding: "5px", ...style}} {...props}>
+    //   {createArray(totalStars).map((n,i) => (
+    //     <Star
+    //     key={i}
+    //     selected={selectedStars > i }
+    //     onSelect={() => setSelectedStars(i +1)}
+    //     />
+    //   ))}
+    //   <p>{selectedStars} of {totalStars} stars</p>
+    // </div>
+  );
 }
