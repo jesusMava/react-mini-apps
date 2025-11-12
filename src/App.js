82e82e098a1271a3f526a95fcc22604b1cea8b37
jesusMava <jesus.macedo7@gmail.com>
2025-11-12 09:17:47 -0600

    add rating color

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import StarRating from './StarRating';
+import colorData from './color-data.json';
+import { useState } from 'react';
+import ColorList from './ColorList';
 
 function App() {
+  const [colors] = useState(colorData)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          and save to reload.
+        <StarRating totalStars={5} 
+          style={{backgoundColor: "lightblue"}}
+          onDoubleClick={e => alert("Double clicked!")}
+        />
+
+        <ColorList colors={colors} />
+
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <StarRating totalStars={5} />
-        </a>
       </header>
+
     </div>
   );
 }
