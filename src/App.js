83d044678948a231067b2242ea2e024273ced466
jesusMava <jesusmacedo7@gmail.com>
2025-11-26 10:55:27 -0600

    add eslint, and prettier

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-import StarRating from './StarRating';
-import colorData from './color-data.json';
-import { useState } from 'react';
-import ColorList from './ColorList';
-import AddColorForm from './AddColorForm';
-import { v4 } from 'uuid';
-import CheckBox from './Checkbox';
+import logo from "./logo.svg";
+import "./App.css";
+import StarRating from "./StarRating";
+import colorData from "./color-data.json";
+import { useState } from "react";
+import ColorList from "./ColorList";
+import AddColorForm from "./AddColorForm";
+import { v4 } from "uuid";
+import CheckBox from "./Checkbox";
+import { GraphQLClient } from "graphql-request";
 
 function App() {
-  const [colors, setColors] = useState(colorData)
+  const [colors, setColors] = useState(colorData);
+
+  const query = `
+    query findRepos($login:String!) {
+      user(login:$login) {
+        login
+        name
+        location
+        avatar_url: avatarUrl
+        repositories(first:100) {
+          totalCount
+          nodes {
+            name
+          }
+        }
+      }
+    }
+    `;
+
+    const client = new GraphQLClient(
+      "https://api.github.com/graphql",
+      {
+        headers: {
+          Authorization: `Bearer <PERSONAL_ACCESS_TOKEN>`
+        }
+      }
+      );
+
+      client
+        .request(query, { login: "moontahoe" })
+        .then(results => JSON.stringify(results, null, 2))
+        .then(console.log)
+        .catch(console.error);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          and save to reload.
-          <CheckBox />
+        and save to reload.
+        <CheckBox />
         {/* <StarRating totalStars={5} 
           style={{backgoundColor: "lightblue"}}
           onDoubleClick={e => alert("Double clicked!")}
@@ -25,9 +58,7 @@ function App() {
         <ColorList />
 
         <AddColorForm /> */}
-        
       </header>
-
     </div>
   );
 }
---
+++ src/sample.js
@@ -0,0 +1,15 @@
+const gnar = "gnarly";
+const info = ({
+file =__filename,
+dir = __dirname
+}) => (
+<p>
+{dir}: {file}
+</p>
+);
+
+switch (gnar) {
+default:
+console.log("gnarly");
+break;
+}
